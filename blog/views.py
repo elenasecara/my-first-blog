7f1e016e9da764466d6aee3f3efee94df89ffc96
Elena-Alexandra Secara <e.a.secara@edu.salford.ac.uk>
2021-12-15 11:16:46 +0000

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
+import xlsxwriter
+
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-import xlsxwriter
 
 # Create your views here.
 def post_list(request):
