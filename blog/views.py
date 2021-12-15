f5dd02faee7ea3141e1528629774ba94d6d40ec4
Elena-Alexandra Secara <e.a.secara@edu.salford.ac.uk>
2021-12-15 11:09:57 +0000

    fix function to throw error

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+import xlsxwriter
 
 # Create your views here.
 def post_list(request):
@@ -13,10 +14,6 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
-def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -24,6 +21,10 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            try:
+                write_to_spreadsheet()
+            except:
+                return render(request, 'blog/post_edit.html', {'form': form})
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm
@@ -42,3 +43,22 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def write_to_spreadsheet():
+    print("trying to write to spreadsheet")
+    # the file to be created
+    workbook = xlsxwriter.Workbook('C:/Users/sgb584/Desktop/booking.xlsx')
+
+    # add new worksheet
+    worksheet = workbook.add_worksheet()
+
+    row = 0
+    column = 0
+
+    content = ["ankit", "rahul", "priya", "harshita",
+                    "sumit", "neeraj", "shivam"]
+
+    for item in content:
+        worksheet.write(row, column, item)
+        row += 1
+    workbook.close()
